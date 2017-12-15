d0e20be6c99ba80bd229cac93f0753143e71bcca
gaurangdobariya <dobariyagaurang@gmail.com>
2017-12-15 16:36:38 +0530

    initiL COMMIT

--- app/src/main/java/com/busman/busman/student.java
+++ app/src/main/java/com/busman/busman/student.java
@@ -5,15 +5,18 @@ package com.busman.busman;
  */
 
 public class student {
-    String id,name;
+    String id,name,stop,fees;
 
     public student(){
 
     }
 
-    public student(String id, String name) {
+    public student(String id, String name, String stop, String fees) {
+
         this.id = id;
         this.name = name;
+        this.stop = stop;
+        this.fees = fees;
     }
 
     public String getId() {
@@ -22,5 +25,13 @@ public class student {
 
     public String getName() {
         return name;
+    }
+
+    public String getStop() {
+        return stop;
+    }
+
+    public String getFees() {
+        return fees;
     }
 }

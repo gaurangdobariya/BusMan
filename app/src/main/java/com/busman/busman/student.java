5b7dfbb798cf0273e46f9ab45d2caf8652e930ca
gaurangdobariya <dobariyagaurang@gmail.com>
2017-12-15 14:56:24 +0530

    initiL COMMIT

---
+++ app/src/main/java/com/busman/busman/student.java
@@ -0,0 +1,26 @@
+package com.busman.busman;
+
+/**
+ * Created by gaurang on 15-12-2017.
+ */
+
+public class student {
+    String id,name;
+
+    public student(){
+
+    }
+
+    public student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

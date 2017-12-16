dc56ec4153ad0a2020d39c89038b5cae8a4188ab
gaurangdobariya <dobariyagaurang@gmail.com>
2017-12-16 12:39:46 +0530

    initiL COMMIT

--- app/src/main/java/com/busman/busman/MainActivity.java
+++ app/src/main/java/com/busman/busman/MainActivity.java
@@ -1,22 +1,26 @@
 package com.busman.busman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     DatabaseReference databaseReference;
 
     EditText id_no,name,fee;
-    Button ok;
+    Button ok,displayb;
     Spinner sp;
 
     @Override
@@ -32,13 +36,23 @@ public class MainActivity extends AppCompatActivity {
        sp= (Spinner) findViewById(R.id.stop);
         fee= (EditText) findViewById(R.id.fees);
         ok = (Button) findViewById(R.id.ok);
-
+        displayb= (Button) findViewById(R.id.display);
         ok.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 senddata();
+
             }
         });
+        displayb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,display_list.class);
+                startActivity(i);
+
+            }
+        });
+
     }
     public void senddata(){
         String id = id_no.getText().toString();
@@ -57,4 +71,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"enter data",Toast.LENGTH_SHORT).show();
         }
     }
+
 }
